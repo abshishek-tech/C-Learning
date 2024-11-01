0ef5f21575ba6b4ae4237b49387afd62b5b0ba6f
abhishek-tech <agrawalabhi022@gmail.com>
2024-11-02 02:42:05 +0530

    solved happy number.c

--- PracticeC/happynumber.c
+++ PracticeC/happynumber.c
@@ -1,2 +1,32 @@
 // find whether given number is a happy number or not. In number theory, a happy number is a number which eventually reaches 1 when replaced by the sum of the square of each digit
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    //take input
+    int a;
+    printf("test number: ");
+    scanf("%d",&a);
 
+    //to check for happy number
+    int k=a;
+    int sum=0;
+    for(int i,o=0;;o++)
+    {   i=k%10;
+        
+        sum=sum+pow(i,2);
+        k=k/10;
+        if(k==0)
+        {
+            k=sum;
+            if(sum==1){printf("happy number \n");break;}
+            if(sum==a){printf("not happy number\n");break;}
+            sum=0;
+        }
+        if(o==50){printf("not happy number\n");break;}
+
+
+    }
+    return 0;
+
+}
