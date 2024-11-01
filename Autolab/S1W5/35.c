a978c2f028e7d8f10f1b947ae7ed833424693ff1
abhishek-tech <agrawalabhi022@gmail.com>
2024-11-01 16:57:57 +0530

    solved 35.c

--- Autolab/S1W5/35.c
+++ Autolab/S1W5/35.c
@@ -1,6 +1,12 @@
 // find all the numbers divisible by 3 and 5 between o and n
 # include<stdio.h>
 int main() {
+    int n,b=15;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+        {if(i%b==0)
+        {printf("%d ,",i);}
+        }   
 
     return 0;
 }
