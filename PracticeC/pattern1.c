a86fffcfecd8d8310c86685ca6e77d3c334c8fc7
abhishek-tech <agrawalabhi022@gmail.com>
2024-11-02 15:44:38 +0530

    solved pattern1.c

--- PracticeC/pattern1.c
+++ PracticeC/pattern1.c
@@ -5,3 +5,22 @@
 ****
 *****
 */ 
+# include<stdio.h>
+int main()
+{
+    // take how many rows to be taken
+    int rows;
+    printf("input number of rows: ");
+    scanf("%d",&rows);
+
+    //code it out 
+    for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<=i;j++)       //print *
+                {
+                    printf("* ");       
+                }
+            printf("\n");               //change line
+        }
+    return 0;
+}
