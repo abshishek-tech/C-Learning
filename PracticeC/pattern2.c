53abe0b8c30ed9ef5566c8bb5d1cabc34ebaf6e4
abhishek-tech <agrawalabhi022@gmail.com>
2024-11-02 15:44:56 +0530

    solved pattern2.c

--- PracticeC/pattern2.c
+++ PracticeC/pattern2.c
@@ -5,3 +5,30 @@ print the following kind of pattern
  * * *
 * * * *
 */
+
+# include<stdio.h>
+int main()
+{
+    // take how many rows to be taken
+    int rows;
+    printf("input number of rows: ");
+    scanf("%d",&rows);
+
+    //code it out 
+    for(int i=0;i<rows;i++)
+        {
+            //print the space bars(= number of rows-row number)
+            for(int k=0;;k++)
+                {   
+                    if(k==rows-i){break;}
+                    printf(" ");
+                }
+            //print the * required
+            for(int j=0;j<=i;j++)
+                {
+                    printf("* ");
+                }
+            printf("\n");
+        }
+    return 0;
+}
