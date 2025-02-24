e2d057006c156a3b6d4d72a1deab9b5d14dbac0b
abhishek-tech <agrawalabhi022@gmail.com>
2025-02-24 23:38:21 +0530

    solved Palindrome.c

--- PracticeC/Palindrome.c
+++ PracticeC/Palindrome.c
@@ -1,17 +1,29 @@
-// Find whether the Binary representation of the given decimal is a palindrome or not
-# include<stdio.h>
-# include<math.h>
+// C++ Program to Check if binary representation 
+// of a number is palindrome 
+#include <stdio.h>
+# include<math.h> 
 
-int main(){
+// Function to check if binary representation is a palindrome
+int checker(int x) {
+    int n = ((log(x))/(log(2)) + 1); // Find number of bits
+    int l = 0, r = n - 1;
 
-unsigned long long int a,sum=0,b=0,c,d=10;
-scanf("%lld",&a);
-for(int i=0;a!=0;a=a/2,i++){
-  b = a%2;
-  c=(pow(d,i))*b;
-  sum = sum+c;
+    while (l < r) {
+        // Compare bits at positions l and r
+        if (((x >> l) & 1) != ((x >> r) & 1)) {
+            return 0;
+        }
+        l++;
+        r--;
+    }
+    return 1;
 }
-printf("%lld", sum);
 
-return 0;
+int main() {
+    int n;
+    scanf("%d",&n);
+    int gate=checker(n);
+    if(gate==1){printf("Palindrome\n");}
+    else{printf("Not Palindrome");}
+    return 0;
 }
